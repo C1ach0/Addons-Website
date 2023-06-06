3294e3ba0fe390c0ff0879c88d296867f69730e8
C1ach0 <c1ach83500@gmail.com>
2023-06-06 09:44:50 +0200

    toggle Class

---
+++ src/class/Toggle.class.ts
@@ -0,0 +1,33 @@
+export default class Toggle {
+    private tog: boolean;
+
+    /**
+     * @param {boolean} v Default state of boolean
+     */
+    constructor(v: boolean) {
+        this.tog = v;
+    }
+    
+    /**
+     * Get if boolean is true or false
+     */
+    public get isToggled(): boolean {
+        return this.tog;
+    }
+    
+    /**
+     * Switch between true/false
+     */
+    public switch() {
+        this.tog = !this.tog;
+    }
+
+    /**
+     * Change manually state of boolean
+     * @param {boolean} v 
+     */
+    public setToggle(v: boolean) {
+        this.tog = v;
+    }
+    
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Toggle from "./class/Toggle.class";
+
+export {
+    /**
+     * Create new instance for boolean, easy to change between true/false
+     */
+    Toggle,
+
+
+}
